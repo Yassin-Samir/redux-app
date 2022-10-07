95136d2e8d7a3d024f23111892103eaf1215a69c
yassin samir <yassinsam3@gmail.com>
2022-10-07 16:33:28 +0200

    fixing bugs + new changes + build folder

--- src/app.js
+++ src/app.js
@@ -3,31 +3,47 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { sendData, start } from "./redux/slice";
 export default function App() {
-  let input;
-  React.useEffect(() => {
-    input = document.querySelector("input[type='text']");
-  }, []);
+  const input = React.useRef(null);
+  const span = React.useRef(null);
+  let [num, setNum] = React.useState(4);
   const handle = (e) => {
     e.preventDefault();
-    dis(start());
-    input.setAttribute("readonly", true);
-    setTimeout(() => {
-      dis(
-        sendData({
-          name: input.value,
-        })
-      );
-      input.value = "";
-      input.removeAttribute("readonly");
-    }, 3000);
+    if (input.current.value) {
+      span.current.classList.add("hidden");
+      span.current.classList.remove("visible");
+      input.current.classList.remove("shake");
+      dis(start());
+      input.current.setAttribute("readonly", true);
+      setTimeout(() => {
+        dis(
+          sendData({
+            name: input.current.value,
+          })
+        );
+        input.current.value = "";
+        input.current.removeAttribute("readonly");
+        setNum(4);
+      }, 5000);
+      const interval = setInterval(() => {
+        if (num) setNum(num--);
+        else clearInterval(interval);
+      }, 1000);
+    } else {
+      span.current.classList.remove("hidden");
+      span.current.classList.add("visible");
+      input.current.classList.add("shake");
+    }
   };
   const dis = useDispatch();
   const { loading } = useSelector((state) => state);
   return (
     <form onSubmit={handle}>
-      <input type="text" placeholder="Enter name" />
+      <input type="text" placeholder="Enter name" ref={input} />
+      <span className="span" ref={span}>
+        required
+      </span>
       {loading ? (
-        <p style={{ width: "100%", textAlign: "center" }}>Loading</p>
+        <p style={{ width: "100%", textAlign: "center" }}>{num}</p>
       ) : (
         <input type="submit" value="submit" />
       )}
