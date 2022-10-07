ef03885fd5697d526d7a91ad7f5d50c7472a9454
yassin samir <yassinsam3@gmail.com>
2022-10-07 02:01:01 +0200

    removing unwanted code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import "./css/main.css";
 import Nav from "./components/nav/nav";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 let main = ReactDOM.createRoot(document.querySelector("#root"));
 main.render(
   <React.StrictMode>
